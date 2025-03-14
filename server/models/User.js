048233cca0e573574f0474011ef7e7e4be99897c
Madhav-Sarma <a.madhavsarma@gmail.com>
2025-03-14 15:30:58 +0530

    this is better than before add ui and some feature

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,61 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function(v) {
+        // Regex to validate institutional email addresses
+        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(edu|ac\.[a-zA-Z]{2,})$/.test(v);
+      },
+      message: props => `${props.value} is not a valid institutional email!`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  fullName: {
+    type: String,
+    required: true
+  },
+  rollNumber: {
+    type: String,
+    required: true
+  },
+  institution: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['student', 'faculty', 'admin'],
+    default: 'student'
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Method to check password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
